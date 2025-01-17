6f8638502495795c8987c605d9ffb1e3ea6c9b47
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-01-17 14:15:19 +0000

    Header component to make the code leaner. Contains router links to three pages.

---
+++ src/components/header/index.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/html-closing-bracket-newline -->
+<!-- eslint-disable vue/html-indent -->
+<template>
+    <v-app-bar title="Resume Work">
+      <div class="d-flex align-center gap-4">
+        <router-link
+          to="/"
+          class="text-decoration-none text-white mx-2"
+        >
+          Home
+        </router-link>
+  
+        <router-link
+          to="/about"
+          class="text-decoration-none text-white mx-2"
+        >
+          About
+        </router-link>
+
+        <router-link
+          to="/sign-in"
+          class="text-decoration-none text-white mx-2"
+        >
+          Sign In
+        </router-link>
+      </div>
+    </v-app-bar>
+  </template>
