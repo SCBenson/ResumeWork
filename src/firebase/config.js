e3a56c1a33f6e25fe7ee641b9c9dfd63560a6f6c
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-01-17 14:36:13 +0000

    added a firebase folder to store a config.js file that will initialize the firebase app.

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCGoisv9H1CiKUgXte0eiAD7v3owJ0Gz2E",
+  authDomain: "resume-work-68782.firebaseapp.com",
+  projectId: "resume-work-68782",
+  storageBucket: "resume-work-68782.firebasestorage.app",
+  messagingSenderId: "1007768208860",
+  appId: "1:1007768208860:web:483de708933e2a5b81fb19",
+  measurementId: "G-8LFFZWM3LM",
+};
+
+initializeApp(firebaseConfig);
