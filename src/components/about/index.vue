0e78514cae8e16f3713d8cbdff9dab812c12b428
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-01-17 14:14:27 +0000

    initiaed an about page and imported the header component.

---
+++ src/components/about/index.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/html-closing-bracket-spacing -->
+<!-- eslint-disable vue/html-indent -->
+<template>
+    <Header/>
+</template>
+
+<script setup>
+    import Header from '@/components/header/index.vue';
+</script>
+
+
+<style>
+</style>
