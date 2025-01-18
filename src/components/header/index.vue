8fc0c7b8c4c46b97321dea59a8d3a12c3f6de3a1
Sean Benson Guerra <sean_c_benson@outlook.com>
2025-01-18 15:22:50 +0000

    imported an SVG from the assets folder to be placed as an src for an image tag in the v-app-bar section.

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,28 +1,34 @@
+<!-- eslint-disable vue/html-self-closing -->
+<!-- eslint-disable vue/max-attributes-per-line -->
 <!-- eslint-disable vue/html-closing-bracket-newline -->
 <!-- eslint-disable vue/html-indent -->
+<script setup>
+import logoUrl from "@/assets/resumework-logo.svg";
+</script>
 <template>
-    <v-app-bar title="Resume Work">
-      <div class="d-flex align-center gap-4">
-        <router-link
-          to="/"
-          class="text-decoration-none text-white mx-2"
-        >
-          Home
-        </router-link>
-  
-        <router-link
-          to="/about"
-          class="text-decoration-none text-white mx-2"
-        >
-          About
-        </router-link>
+  <v-app-bar color="white">
+    <img :src="logoUrl" class="app-bar-logo" alt="Company logo" />
+    <div class="d-flex align-center gap-4">
+      <router-link to="/" class="text-decoration-none text-black mx-2">
+        Home
+      </router-link>
 
-        <router-link
-          to="/sign-in"
-          class="text-decoration-none text-white mx-2"
-        >
-          Sign In
-        </router-link>
-      </div>
-    </v-app-bar>
-  </template>
+      <router-link to="/about" class="text-decoration-none text-black mx-2">
+        About
+      </router-link>
+
+      <router-link to="/sign-in" class="text-decoration-none text-black mx-2">
+        Sign In
+      </router-link>
+    </div>
+  </v-app-bar>
+</template>
+
+<style scoped>
+.app-bar-logo {
+  height: 200px; /* Adjust this value based on your app-bar height */
+  width: auto;
+  max-width: 200px; /* Adjust maximum width as needed */
+  display: block;
+}
+</style>
