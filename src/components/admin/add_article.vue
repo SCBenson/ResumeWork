bb48412a0b2a1850bf2e436c35307bbea5a00fe7
Sean Benson <87237302+SCBenson@users.noreply.github.com>
2025-01-19 23:11:57 +0000

    implemented a form template which submits two text fields to the firestore database. This is then shown in the home page view as a card element.

--- src/components/admin/add_article.vue
+++ src/components/admin/add_article.vue
@@ -1,23 +1,42 @@
 <!--eslint-disable--> 
 <template>
+    <Header/>
     <v-container>
-        <h1>Add an article</h1>
-        <v-row>
-            <v-col col="12">
-                <v-form @submit.prevent="submitForm">
-                    <v-row>
-                        <v-col col="12">
-                            <v-text-field label="Title" v-model="title">
+        <v-alert
+            v-if="showAlert"
+            type="warning"
+            variant="tonal"
+            class="mb-4"
+        >
+        There was a problem submitting your message.
+        </v-alert>
+        <v-row justify="center">
+            <v-col cols="12" md="6">
+                <v-card class="pa-6 custom-border">
+                    <h1 class="text-red">Please add an article</h1>
+                    <p class="pb-2">This information will be sent to the firestore database.</p>
+                    <v-form @submit.prevent="submitForm">
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field label="Title" v-model="formData.title">
 
-                            </v-text-field>
-                        </v-col>
-                        <v-col>
-                            <v-textarea v-model="description">
+                                </v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-textarea label="Description" v-model="formData.description">
 
-                            </v-textarea>
-                        </v-col>
-                    </v-row>
-                </v-form>
+                                </v-textarea>
+                            </v-col>
+                        </v-row>
+                        <v-row class="pl-3">
+                            <v-btn  type="submit" color="primary">
+                                Submit
+                            </v-btn>
+                        </v-row>
+                    </v-form>
+                </v-card>
             </v-col>
         </v-row>
     </v-container>
@@ -26,22 +45,33 @@
 <script setup>
     import {DB} from '@/firebase/config';
     import {collection, addDoc} from 'firebase/firestore';
-    import {reactive} from 'vue';
+    import {ref, reactive} from 'vue';
 
+    const showAlert = ref(false);
     const formData = reactive({
         title: '',
         description: ''
     });
 
     const submitForm = async() => {
-        const notesCollection = collection(DB, 'notes');
+        try{
+            const notesCollection = collection(DB, 'notes');
 
-        await addDoc(notesCollection, {
-            ...formData
-        });
-        //reset the variables after extracting the submission.
-        formData.title = '';
-        formData.description = '';
+            await addDoc(notesCollection, {
+                ...formData
+            });
+            //reset the variables after extracting the submission.
+            formData.title = '';
+            formData.description = '';
+        } catch (error) {
+            showAlert.value = true;
+            console.log('Error submitting the article: ', error);
+        }
     }
 
 </script>
+<style scoped>
+.custom-border {
+    border: 2px solid rgb(178, 2, 2) !important;
+}
+</style>
